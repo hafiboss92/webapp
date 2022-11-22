b278a0e6e4a6371821aea116cfb0b3d91d562aa9
jagabano <hafimans@gmail.com>
2022-11-22 12:37:44 +0000

    first commit

---
+++ app.java
@@ -0,0 +1,5 @@
+# may Allah forgive our beloved shk ubeidi Al jabir
+# we ask Allah to grant noor in his qabur
+# we Allah to preserve our shks who still alive 
+# we ask Allah to make us steadyfast upon  the suunah
+#
