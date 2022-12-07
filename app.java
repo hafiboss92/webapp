42b8049ed2071ef6ba9025e8b0ac80935f13f4bf
salafi30 <118642119+salafi30@users.noreply.github.com>
2022-12-06 19:08:14 -0500

    Delete app.java

--- app.java
+++
@@ -1,5 +0,0 @@
-# may Allah forgive our beloved shk ubeidi Al jabir
-# we ask Allah to grant noor in his qabur
-# we ask Allah to preserve our shks who are still alive 
-# we ask Allah to make us steadyfast upon  the suunah
-#
